b84b2f4a077f1137c95580c382cb65b15986275f
Lucas Mesquita <lucas.mesquita1110@gmail.com>
2023-07-11 11:26:52 -0300

    security change

--- send_email.py
+++ send_email.py
@@ -1,21 +1,16 @@
 import smtplib, ssl
 
-host = "smtp.gmail.com"
-port = 465
 
-username = "lucas.mesquita1110@gmail.com"
-password = "noypxcjzddhpmkro"
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
 
-receiver = "lucas.mesquita1110@gmail.com"
-context = ssl.create_default_context()
+    username = "lucas.mesquita1110@gmail.com"
+    password = ""
 
-message = """\
-Subject: First e-mail automation
-Hi!
-How are you?
-Bye!
-"""
+    receiver = "lucas.mesquita1110@gmail.com"
+    context = ssl.create_default_context()
 
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
